100cabae4d1e9ddf7e372176decfa552985a4900
Federico Teotini <templarblack.fed03@gmail.com>
2018-01-14 20:12:25 +0100

    Extracted common methods

---
+++ src/main/java/com/fed03/ann/ByteFileReader.java
@@ -0,0 +1,55 @@
+package com.fed03.ann;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+public abstract class ByteFileReader<T> implements AutoCloseable {
+    private static final int STEP_SIZE = 4;
+    private static final int BUFFER_SIZE = 32768;
+    int numberOfFields;
+    private InputStream inputStream;
+
+    ByteFileReader(String filename) {
+        try {
+            inputStream = new BufferedInputStream(new FileInputStream(getFile(filename)), BUFFER_SIZE);
+            inputStream.mark(2);
+            numberOfFields = inputStream.read();
+            inputStream.reset();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        inputStream.close();
+    }
+
+    public int getNumberOfFields() {
+        return numberOfFields;
+    }
+
+    byte[] nextRawLine() throws IOException {
+        byte[] components = new byte[numberOfFields * STEP_SIZE];
+        inputStream.skip(STEP_SIZE);
+        if (inputStream.read(components) == -1) {
+            throw new IOException("End of stream");
+        }
+        return components;
+    }
+
+    ByteBuffer createByteBuffer(byte[] components, int index) {
+        return ByteBuffer.wrap(components, index * STEP_SIZE, STEP_SIZE).order(ByteOrder.LITTLE_ENDIAN);
+    }
+
+    protected abstract T getFieldValueByIndex(byte[] components, int index);
+
+    private static String getFile(String filename) {
+        return ClassLoader.getSystemClassLoader().getResource(filename).getFile();
+    }
+}
--- src/main/java/com/fed03/ann/FvecReader.java
+++ src/main/java/com/fed03/ann/FvecReader.java
@@ -2,30 +2,13 @@ package com.fed03.ann;
 
 import org.apache.commons.math3.linear.ArrayRealVector;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FvecReader implements AutoCloseable {
-    private static final int BUFFER_SIZE = 32768;
-    private int vectorDimension;
-    private InputStream inputStream;
-
+public class FvecReader extends ByteFileReader<Float> {
     public FvecReader(String filename) {
-        try {
-            InputStream inputStream = new BufferedInputStream(new FileInputStream(getFile(filename)), BUFFER_SIZE);
-            this.inputStream = inputStream;
-            inputStream.mark(2);
-            vectorDimension = inputStream.read();
-            inputStream.reset();
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
+        super(filename);
     }
 
     public List<ArrayRealVector> getAllVectors() {
@@ -49,30 +32,22 @@ public class FvecReader implements AutoCloseable {
     }
 
     public ArrayRealVector getNextVector() throws IOException {
-        byte[] components = new byte[getVectorDimension() * 4];
-        inputStream.skip(4);
-        if (inputStream.read(components) == -1) {
-            throw new IOException("End of stream");
-        }
+        byte[] components = nextRawLine();
 
         ArrayRealVector vector = new ArrayRealVector(getVectorDimension());
         for (int i = 0; i < getVectorDimension(); i++) {
-            float value = ByteBuffer.wrap(components, i * 4, 4).getFloat();
+            float value = getFieldValueByIndex(components, i);
             vector.setEntry(i, value);
         }
         return vector;
     }
 
-    public int getVectorDimension() {
-        return vectorDimension;
-    }
-
     @Override
-    public void close() throws IOException {
-        inputStream.close();
+    protected Float getFieldValueByIndex(byte[] components, int i) {
+        return createByteBuffer(components, i).getFloat();
     }
 
-    private static String getFile(String filename) {
-        return ClassLoader.getSystemClassLoader().getResource(filename).getFile();
+    private int getVectorDimension() {
+        return numberOfFields;
     }
 }
