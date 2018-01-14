18eadda1db4a39a708c94178d5ae72c7fb0c8785
Federico Teotini <templarblack.fed03@gmail.com>
2018-01-14 11:45:22 +0100

    Finished hash function

--- src/main/java/EuclideanHashFunction.java
+++ src/main/java/EuclideanHashFunction.java
@@ -7,16 +7,27 @@ public class EuclideanHashFunction implements Function<ArrayRealVector, Integer>
     private final double w;
     private final int vectorDimension;
     private final double offset;
+    private final ArrayRealVector randomProjection;
 
     public EuclideanHashFunction(double w, int vectorDimension) {
         this.w = w;
         this.vectorDimension = vectorDimension;
         this.offset = calcOffset(w);
-
+        this.randomProjection = calcRandomProjection();
     }
 
     public Integer apply(ArrayRealVector vector) {
-        return null;
+        return (int) Math.floor((vector.dotProduct(randomProjection) + offset) / w);
+    }
+
+    private ArrayRealVector calcRandomProjection() {
+        ArrayRealVector projection = new ArrayRealVector(vectorDimension);
+        Random rand = new Random();
+        for (int i = 0; i < vectorDimension; i++) {
+            projection.setEntry(i, rand.nextGaussian());
+        }
+
+        return projection;
     }
 
     private static double calcOffset(double w) {
